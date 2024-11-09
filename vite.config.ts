edbcc72e96389eb6376cf50d822879cf470fa254
Anton Loiko <anton.loiko.engineer@gmail.com>
2024-11-09 11:36:16 -0600

    buid(): add configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite"
-import path from "path"
 import react from "@vitejs/plugin-react"
 import svgr from "@svgr/rollup"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr({ include: ["src/assets/*"] })],
-  resolve: {
-    alias: {
-      $components: path.resolve("./src/components"),
-    },
-  },
 })
